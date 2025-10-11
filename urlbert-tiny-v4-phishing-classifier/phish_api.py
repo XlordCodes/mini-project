e2c9403852622f1e702a510d44c11d8e336675ec
XlordCodes <133195042+XlordCodes@users.noreply.github.com>
2025-10-11 11:14:42 +0530

    Updated phish_api.py

--- urlbert-tiny-v4-phishing-classifier/phish_api.py
+++ urlbert-tiny-v4-phishing-classifier/phish_api.py
@@ -23,7 +23,7 @@ class ModelLoader:
     def __init__(self):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         # Update path to use raw string and correct directory
-        self.model_path = r"D:\Muksid\KCG-College\A1-Machine-Learning-Project\MINI-PROJECT\urlbert-tiny-v4-phishing-classifier\model.safetensors"
+        self.model_path = "./"
         self.load_model()
 
     def load_model(self):
